6ad191c1ba4246a37eb1a877acd8a76f235ee7cf
Branislav Vezilic <banex93@gmail.com>
2018-04-09 18:52:55 +0200

    Added test.py

--- test.py
+++ test.py
@@ -1,39 +1,68 @@
-import random
-from data import NucleusDataset, Rescale, ToTensor, Normalize
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
-import cv2
 from torchvision import transforms
-from torch.utils.data.sampler import RandomSampler
+from torch.utils.data import DataLoader
 from torch.autograd import Variable
+
+from data import NucleusDataset, Rescale, ToTensor, Normalize
 
 
 def test():
-    random_sampler = RandomSampler(NucleusDataset('data', train=False,
-                                                  transform=transforms.Compose([
-                                                      Normalize(),
-                                                      Rescale(256),
-                                                      ToTensor()
-                                                  ])))
+    test_loader = DataLoader(NucleusDataset('data',
+                                            train=False,
+                                            transform=transforms.Compose([
+                                                Normalize(),
+                                                Rescale(256),
+                                                ToTensor()])),
+                             batch_size=12,
+                             shuffle=True)
 
-    data_set = NucleusDataset('data', train=False,
-                                                  transform=transforms.Compose([
-                                                      Normalize(),
-                                                      Rescale(256),
-                                                      ToTensor()
-                                                  ]))
     model = torch.load("models/model.pt")
     model.eval()
-    if torch.cuda.is_available():
+    use_gpu = torch.cuda.is_available()
+    if use_gpu:
         model.cuda()
 
-    # for idx, image in enumerate(random_sampler):
-    image = data_set[7]
-    image = image.unsqueeze(0)
-    image = Variable(image.cuda())
-    output = model(image)
-    cv2.imshow('ouput', output.data.cpu().numpy().squeeze())
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    images = next(iter(test_loader))
+    if use_gpu:
+        images = images.cuda()
+    images = Variable(images)
+    outputs = model(images)
+
+    images = tensor_to_numpy(images)
+    outputs = tensor_to_numpy(outputs)
+
+    show_images(images, outputs)
+
+
+def tensor_to_numpy(tensor):
+    t_numpy = tensor.data.cpu().numpy()
+    t_numpy = np.transpose(t_numpy, [0, 2, 3, 1])
+    t_numpy = np.squeeze(t_numpy)
+
+    return t_numpy
+
+
+def show_images(images, masks, columns=6):
+    fig = plt.figure()
+    rows = np.ceil((images.shape[0] + masks.shape[0]) / columns)
+    index = 1
+    for image, mask in zip(images, masks):
+        f1 = fig.add_subplot(rows, columns, index)
+        f1.set_title('input')
+        plt.axis('off')
+        plt.imshow(image)
+        index += 1
+
+        f2 = fig.add_subplot(rows, columns, index)
+        f2.set_title('prediction')
+        plt.axis('off')
+        plt.imshow(mask)
+        index += 1
+
+    plt.show()
+
 
 if __name__ == "__main__":
     test()
